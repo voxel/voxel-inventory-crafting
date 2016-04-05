55be227ca3a82857172962d10d99017159c46d88
deathcap <deathcap@gmx.co.uk>
2016-04-04 20:20:22 -0700

    Convert to native ES6, remove CoffeeScript

--- inventory-crafting.js
+++ inventory-crafting.js
@@ -1,67 +1,90 @@
+'use strict';
 
-Inventory = require 'inventory'
-InventoryWindow = require 'inventory-window'
-ItemPile = require 'itempile'
-InventoryDialog = (require 'voxel-inventory-dialog').InventoryDialog
+const Inventory = require('inventory');
+const InventoryWindow = require('inventory-window');
+const ItemPile = require('itempile');
+const InventoryDialog = require('voxel-inventory-dialog').InventoryDialog;
 
-module.exports = (game, opts) ->
-  new InventoryCrafting(game, opts)
+module.exports = (game, opts) => new InventoryCrafting(game, opts);
 
-module.exports.pluginInfo =
+module.exports.pluginInfo = {
   'loadAfter': ['voxel-recipes', 'voxel-carry', 'voxel-registry']
+};
 
-class InventoryCrafting extends InventoryDialog
-  constructor: (@game, opts) ->
-    @recipes = game.plugins?.get('voxel-recipes') ? throw new Error('voxel-inventory-crafting requires "voxel-recipes" plugin')
-    @registry = game.plugins?.get('voxel-registry') ? throw new Error('voxel-inventory-crafting requires "voxel-registry" plugin')
-    playerInventory = game.plugins?.get('voxel-carry')?.inventory ? opts.playerInventory ? throw new Error('voxel-inventory-crafting requires "voxel-carry" plugin')
+class InventoryCrafting extends InventoryDialog {
+  constructor(game, opts) {
 
-    @craftInventory = new Inventory(2, 2)
-    @craftInventory.on 'changed', () => @updateCraftingRecipe()
-    @craftIW = new InventoryWindow {inventory:@craftInventory, registry:@registry, linkedInventory:playerInventory}
+    const recipes = game.plugins.get('voxel-recipes');
+    if (!recipes) throw new Error('voxel-inventory-crafting requires "voxel-recipes" plugin')
 
-    @resultInventory = new Inventory(1)
-    @resultIW = new InventoryWindow {inventory:@resultInventory, registry:@registry, allowDrop:false, linkedInventory:playerInventory}
-    @resultIW.on 'pickup', () => @tookCraftingOutput()
+    const registry = game.plugins.get('voxel-registry');
+    if (!registry) throw new Error('voxel-inventory-crafting requires "voxel-registry" plugin')
 
-    # crafting + result div, add to upper from InventoryDialog
-    craftCont = @craftIW.createContainer()
+    const playerInventory = game.plugins.get('voxel-carry').inventory || opts.playerInventory; // TODO: proper error if voxel-carry missing
+    if (!playerInventory) throw new Error('voxel-inventory-dialog requires "voxel-carry" plugin or playerInventory" set to inventory instance');
 
-    craftContOuter = document.createElement 'div'  # so craftCont can float left
-    craftContOuter.appendChild craftCont
 
-    resultCont = @resultIW.createContainer()
-    resultCont.style.display = 'flex'
-    resultCont.style.flexFlow = 'column'
-    resultCont.style.justifyContent = 'center'
-    resultCont.style.marginLeft = '30px'
+    const craftInventory = new Inventory(2, 2);
+    craftInventory.on( 'changed', () => this.updateCraftingRecipe());
+    const craftIW = new InventoryWindow({inventory:craftInventory, registry:registry, linkedInventory:playerInventory});
 
-    outer = document.createElement 'div'
-    outer.style.display = 'flex'
-    outer.style.float = 'right'
-    outer.style.margin = '30px'
+    const resultInventory = new Inventory(1);
+    const resultIW = new InventoryWindow({inventory:resultInventory, registry:registry, allowDrop:false, linkedInventory:playerInventory});
+    resultIW.on('pickup', () => this.tookCraftingOutput());
 
-    outer.appendChild craftContOuter
-    outer.appendChild resultCont
+    // crafting + result div, add to upper from InventoryDialog
+    const craftCont = craftIW.createContainer();
 
-    super game,
+    const craftContOuter = document.createElement('div');  // so craftCont can float left
+    craftContOuter.appendChild(craftCont);
+
+    const resultCont = resultIW.createContainer();
+    resultCont.style.display = 'flex';
+    resultCont.style.flexFlow = 'column';
+    resultCont.style.justifyContent = 'center';
+    resultCont.style.marginLeft = '30px';
+
+    const outer = document.createElement('div');
+    outer.style.display = 'flex';
+    outer.style.float = 'right';
+    outer.style.margin = '30px';
+
+    outer.appendChild(craftContOuter);
+    outer.appendChild(resultCont);
+
+    super(game, {
       upper: [outer]
+    });
 
-  enable: () ->
+    this.game = game;
+    this.recipes = recipes;
+    this.registry = registry;
+    this.craftInventory = craftInventory;
+    this.craftIW = craftIW;
+    this.resultInventory = resultInventory;
+    this.resultIW = resultIW;
+  }
 
-  disable: () ->
+  enable() {
+  }
 
-  # changed crafting grid, so update recipe output
-  updateCraftingRecipe: () ->
-    recipe = @recipes.find(@craftInventory)
-    console.log 'found recipe',recipe
-    @resultInventory.set 0, recipe?.computeOutput(@craftInventory)
+  disable() {
+  }
 
-  # picked up crafting recipe output, so consume crafting grid ingredients
-  tookCraftingOutput: () ->
-    recipe = @recipes.find(@craftInventory)
-    return if not recipe?
+  // changed crafting grid, so update recipe output
+  updateCraftingRecipe() {
+    const recipe = this.recipes.find(this.craftInventory);
+    console.log('found recipe',recipe);
+    this.resultInventory.set(0, recipe !== undefined ? recipe.computeOutput(this.craftInventory) : undefined);
+  }
 
-    recipe.craft(@craftInventory)
-    @craftInventory.changed()
+  // picked up crafting recipe output, so consume crafting grid ingredients
+  tookCraftingOutput() {
+    const recipe = this.recipes.find(this.craftInventory);
+    if (recipe === undefined) return;
+
+    recipe.craft(this.craftInventory);
+    this.craftInventory.changed();
+  }
+}
 
